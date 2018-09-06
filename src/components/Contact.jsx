6dfad0b842ad2a0d87c99af90936ae4900c3196f
Somtochukwu Dibie <somto.db@gmail.com>
2018-09-06 18:13:50 +0100

    Initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
+import Jumbotron from '../components/Jumbotron.jsx';
+
+import './Home.css';
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<Jumbotron title="Contact" subtitle="the contact subtitle" />
+				<div className="container">
+					<h2>Contact</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<p>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+					</p>
+				</div>
+				<Footer />
+			</div>
+		);
+	}
+}
+
+export default Contact;
