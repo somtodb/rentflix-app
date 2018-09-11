b0efd1ed6cb67a5ebde7bc4ad637603bb13e3c33
Somtochukwu Dibie <somto.db@gmail.com>
2018-09-11 01:21:44 +0100

    Initial commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,37 +4,38 @@ import './Carousel.css';
 class Carousel extends Component {
   render() {
     return (
-          <div id="myCarousel" className="carousel slide" data-ride="carousel">
-            
+          <div id="myCarousel" className="carousel slide">
             <div className="carousel-inner">
               <div className="carousel-item active">
                 <div className="row">
                   <div className="col">
-                    <iframe width="450" height="155px"
+                    <iframe width="459" height="155px"
                     src="https://www.youtube.com/embed/UUkn-enk2RU">
                     </iframe>
                   </div>
+                  
                   <div className="col">
                     <iframe width="459" height="155px"
                     src="https://www.youtube.com/embed/xLCn88bfW1o">
                     </iframe>
                   </div>
                   <div className="col">
-                    <iframe width="450" height="155px"
+                    <iframe width="459" height="155px"
                     src="https://www.youtube.com/embed/WDkg3h8PCVU">
                     </iframe>
-                  </div>  
+                  </div>
+                  
                 </div> 
               </div>
               <div className="carousel-item">
                 <div className="row">
                   <div className="col">
-                    <iframe width="460" height="155px"
+                    <iframe width="459" height="155px"
                     src="https://www.youtube.com/embed/14ZHRBfpeNg">
                     </iframe>
                   </div>
                   <div className="col">
-                    <iframe width="450" height="155px"
+                    <iframe width="459" height="155px"
                     src="https://www.youtube.com/embed/coOKvrsmQiI">
                     </iframe>
                   </div>
@@ -46,11 +47,11 @@ class Carousel extends Component {
                 </div> 
               </div>
             </div>
-            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
               <span className="carousel-control-prev-icon" aria-hidden="true"></span>
               <span className="sr-only">Previous</span>
             </a>
-            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
               <span className="carousel-control-next-icon" aria-hidden="true"></span>
               <span className="sr-only">Next</span>
             </a>
